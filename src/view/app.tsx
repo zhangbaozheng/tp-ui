b5afee4eb05ced7ab594551268424a2143766a98
zhyeng <488937163@qq.com>
2022-07-28 01:19:32 +0800

    test

--- src/view/app.tsx
+++ src/view/app.tsx
@@ -2,7 +2,10 @@ import React from "react"
 import Button from "../compontes/button/button"
 const App = () => {
     return (
-        <Button disabled={false}>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</Button>
+        <Button disabled={false}>
+            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
+            累死我了大伯比
+        </Button>
     )
 }
 export default App
